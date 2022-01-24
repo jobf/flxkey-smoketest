e407883a860b392200bf8ef71cfa4c6c4b374f23
jf <j@b.f>
2022-01-24 16:04:54 +0000

    still rudimentary but now with visual feedback

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,12 +2,34 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
+import flixel.tweens.misc.ColorTween;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
+	var textWTF:FlxText;
+	var textANY:FlxText;
+	var textNONE:FlxText;
+	var tweenTextANY:ColorTween;
+	var tweenTextNONE:ColorTween;
+
 	override public function create()
 	{
 		super.create();
+		textWTF = new FlxText(0, 0, FlxG.width, "Press any key to test ANY key was pressed, click mouse to test NONE key was ... not pressed?", 25);
+		add(textWTF);
+
+		textANY = new FlxText(0, 0, 0, "ANY", 50);
+		textANY.screenCenter();
+		textANY.y -= 50;
+		add(textANY);
+
+		textNONE = new FlxText(0, 0, 0, "NONE", 50);
+		textNONE.screenCenter();
+		textNONE.y += 50;
+		add(textNONE);
 	}
 
 	override public function update(elapsed:Float)
@@ -16,10 +38,22 @@ class PlayState extends FlxState
 		if (FlxG.keys.justPressed.ANY)
 		{
 			trace("ANY");
+			if (tweenTextANY != null)
+			{
+				tweenTextANY.cancel();
+				textANY.color = FlxColor.WHITE;
+			}
+			tweenTextANY = FlxTween.color(textANY, 0.33, FlxColor.LIME, FlxColor.WHITE);
 		}
 		if (FlxG.mouse.justPressed && FlxG.keys.justPressed.NONE)
 		{
 			trace("NONE");
+			if (tweenTextNONE != null)
+			{
+				tweenTextNONE.cancel();
+				textNONE.color = FlxColor.WHITE;
+			}
+			tweenTextNONE = FlxTween.color(textNONE, 0.33, FlxColor.LIME, FlxColor.WHITE);
 		}
 	}
 }
